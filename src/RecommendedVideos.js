1e95e2e7cb65f7a3057af84a24480288dfbcd555
Sayed Sadat <SadatSayed92@gmail.com>
2020-10-03 14:35:52 -0700

    Adding Sidebar

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (
+        <div className="RecommendedVideos">
+            <h1>Recommended Videos</h1>
+        </div>
+    );
+}
+
+export default RecommendedVideos;
