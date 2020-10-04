34f5874765d844ba46a5d103908490255d8d06d7
Sayed Sadat <SadatSayed92@gmail.com>
2020-10-04 01:02:38 -0700

    Sidebar Completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,13 +4,28 @@ import SidebarRow from './SidebarRow';
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OnDemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltOutinedIcon from '@material-ui/icons/ThumbUpAlt';
+import ExpandMoreOutlindedIcon from '@material-ui/icons/ExpandMore';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow selected Icon={HomeIcon} title="Home" />
             <SidebarRow Icon={WhatshotIcon} title="Trending" />
             <SidebarRow Icon={SubscriptionIcon} title="Subscription" />
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={OnDemandVideoIcon} title="Your Videos" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={ThumbUpAltOutinedIcon} title="Liked Videos" />
+            <SidebarRow Icon={ExpandMoreOutlindedIcon} title="Show more" />
+            <hr />
         </div>
     );
 }
